471d9c3e21ecbe0344cd14d8334407b47b5c2576
Cameron <hackwithcameron@gamil.com>
2020-03-28 10:25:56 -0700

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS'), 'localhost', '127.0.0.1']
 
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
