9ba309304a5792911ee78f6917cb880d3e3d897d
Cameron <hackwithcameron@gmail.com>
2020-04-21 17:55:52 -0700

    Added Project
    Added Covid project to projects page and also edited "About me" section. Added placeholders for contact form

--- Contact/forms.py
+++ Contact/forms.py
@@ -6,11 +6,12 @@ from django import forms
 class ContactForm(ModelForm):
     message = forms.CharField(max_length=500, widget=forms.Textarea(
         attrs={
-            'placeholder': "Write your message here..."
+            'placeholder': "Let me know how I can help you..."
         }))
-    widgets = {
-        'firstName': forms.TextInput(attrs={'placeholder': "First Name"})
-    }
+    first_Name = forms.CharField(max_length=30, label='First Name', widget=forms.TextInput(attrs={'placeholder': "First Name"}))
+    last_Name = forms.CharField(max_length=30, label='Last Name', widget=forms.TextInput(attrs={'placeholder': "Last Name"}))
+    email_Address = forms.CharField(max_length=50, label='Email Address', widget=forms.EmailInput(attrs={'placeholder': "Email Address"}))
+
     class Meta:
         model = Contact
         exclude = ['date', 'id']
