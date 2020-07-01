8539c883347ee39fa0eb203cc5bfb3781bc8acad
Cameron <hackwithcameron@gmail.com>
2020-07-01 12:24:42 -0700

    Move Project Page
    Moved project page to index page for quicker access.

--- staticfiles/js/portfolio.js
+++ staticfiles/js/portfolio.js
@@ -1,46 +1,46 @@
 
 function animation(x) {
-    var navList = document.getElementById('navbarInfo');
+    let navList = document.getElementById('navbarInfo');
     if (navList.classList.contains('show')){
         x.classList.remove("change");
     } else if (!navList.classList.contains('show')) {
         x.classList.add("change");
     }
-};
+}
 
 //  -------Removes change class from menu hamburger if main document is clicked--------
 document.getElementById('main').addEventListener('click', function(){
-    var nav = document.getElementById('nav-button');
+    let nav = document.getElementById('nav-button');
     nav.classList.remove('change');
 });
 
 //  -------Projects Modals--------
-var modal1 = document.getElementById("modal1");
-var modal2 = document.getElementById("modal2");
-var modal3 = document.getElementById("modal3");
-var navBar = document.getElementById("navBar")
+let modal1 = document.getElementById("modal1");
+let modal2 = document.getElementById("modal2");
+let modal3 = document.getElementById("modal3");
+let navBar = document.getElementById("navBar")
 
-var cards = document.getElementsByClassName('projectCard');
+let cards = document.getElementsByClassName('projectCard');
 
 
 function projectCards(event){
-    for (var i = 0; i < cards.length; i++){
+    for (let i = 0; i < cards.length; i++){
         cards[i].classList.add('move');
     }
     navBar.style.display = "none";
-    if (event.target.id == 'project1'){
+    if (event.target.id === 'project1'){
         modal1.style.display = "block";
     }
-    if (event.target.id == 'project2'){
+    if (event.target.id === 'project2'){
         modal2.style.display = "block";
     }
-    if (event.target.id == 'project3'){
+    if (event.target.id === 'project3'){
         modal3.style.display = "block";
     }
-};
+}
 
 function close1(){
-    for (var i = 0; i < cards.length; i++){
+    for (let i = 0; i < cards.length; i++){
         cards[i].classList.remove('move');
     }
     modal1.style.display = "none";
@@ -48,29 +48,30 @@ function close1(){
     modal3.style.display = "none";
     navBar.style.display = "flex";
 
-};
+}
 
-var img = document.getElementById('');
-var text = document.getElementById('');
+let img = document.getElementById('');
+let text = document.getElementById('');
 
 //  -------Scroll Reveal--------
 
-var config = {
+let config = {
     duration: 2000,
     distance: '50px',
     viewFactor: 0.3
 }
 
-var config2 = {
+const config2 = {
     duration: 2000,
     distance: '50px',
     viewFactor: 0.3,
     origin: 'top'
-}
+};
 
 
 window.sr = ScrollReveal(config);
 sr.reveal('.development');
+sr.reveal('.projectCard');
 sr.reveal('#gradient2');
 
 
@@ -79,8 +80,8 @@ sr.reveal('#gradient');
 
 
 //  -------NavBar Adjustment--------
-var page = window.location.pathname;                // Gets url of current page
-var navContainer = document.getElementById("nav-container");
+let page = window.location.pathname;                // Gets url of current page
+let navContainer = document.getElementById("nav-container");
 
 if(page === '/' || page === '/#aboutLabel/'){           // Checks to see if url is homepage
     navContainer.style.height = 0;              // Sets navbar container height to 0
