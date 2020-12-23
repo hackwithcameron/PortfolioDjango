ed0f9f89ad1fdefe19b5a882c8ffa01ffc6bec30
Cameron <hackwithcameron@gmail.com>
2020-12-23 11:32:33 -0800

    db update

--- Contact/models.py
+++ Contact/models.py
@@ -5,11 +5,10 @@ from django import forms
 class Contact(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True)
-    first_Name = models.CharField(max_length=30, unique=True)
-    last_Name = models.CharField(max_length=30, unique=True)
-    email_Address = models.EmailField(max_length=50, unique=True)
+    first_Name = models.CharField(max_length=30)
+    last_Name = models.CharField(max_length=30)
+    email_Address = models.EmailField(max_length=50)
     message = models.CharField(max_length=250)
-
     Contact = models.Manager()
 
     def __str__(self):
